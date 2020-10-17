81c03bd43a14ca2030da0ac9208dd340534f288a
Hema123 <hemaghg@gmail.com>
2020-10-17 14:31:33 +0200

    Application properties

--- src/main/java/com/example/jpacrud/JpaassignmentApplication.java
+++ src/main/java/com/example/jpacrud/JpaassignmentApplication.java
@@ -1,13 +1,25 @@
 package com.example.jpacrud;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.jpacrud.entity.Ingredient;
+import com.example.jpacrud.util.Measurement;
 
 @SpringBootApplication
-public class JpaassignmentApplication {
+public class JpaassignmentApplication implements CommandLineRunner {
+
+	@Autowired
+	Ingredient ingredient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaassignmentApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+
+
+	}
 }
