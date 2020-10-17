b8a8d3c8ead6f0d5ac4db5eca27a8dad3347d926
Hema123 <hemaghg@gmail.com>
2020-10-17 13:17:36 +0200

    Entity relationship

--- src/main/java/com/example/jpacrud/entity/RecipeInstruction.java
+++ src/main/java/com/example/jpacrud/entity/RecipeInstruction.java
@@ -1,7 +1,36 @@
 package com.example.jpacrud.entity;
 
+import java.util.Objects;
+
 public class RecipeInstruction {
 
+    private String id;
+    private String instructions;
 
+    public RecipeInstruction(String id, String instructions) {
+        this.id = id;
+        this.instructions = instructions;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RecipeInstruction)) return false;
+        RecipeInstruction that = (RecipeInstruction) o;
+        return id.equals(that.id) &&
+                instructions.equals(that.instructions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, instructions);
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeInstruction{" +
+                "id='" + id + '\'' +
+                ", instructions='" + instructions + '\'' +
+                '}';
+    }
 }
