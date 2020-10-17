78a294a421414af895b1d2a33b787129f1c425e0
Hema123 <hemaghg@gmail.com>
2020-10-17 13:20:46 +0200

    Enum measurements

---
+++ src/main/java/com/example/jpacrud/util/Measurement.java
@@ -0,0 +1,25 @@
+package com.example.jpacrud.util;
+
+public enum Measurement {
+
+    DL(100),
+    ML(1),
+    CL(10),
+    KG(1000),
+    HG(100),
+    G(1),
+    TSP(5),
+    TBSP(15);
+
+    private int value;
+
+    Measurement(int value) {
+        this.value = value;
+    }
+
+    public int meas(){
+        return value;
+    }
+
+
+}
